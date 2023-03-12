b0e6af09e9439f52786f4ec41a3dcf5f84a3da0f
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-03-12 14:10:36 +0000

    [Add]Bookers2_user_finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,14 +6,19 @@ class BooksController < ApplicationController
   def index
     @booknew=Book.new
     @books=Book.all
+    @book=Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @books= Book.all
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+   @books= Book.all
+   @book = Book.new(book_params)
+   @book.user_id = current_user.id
+   if @book.save
+      flash[:notice]="You have createded books successfully."
+      redirect_to book_path(@book)
+   else
+     render :index
+   end
   end
 
   def show
@@ -33,8 +38,12 @@ class BooksController < ApplicationController
 
   def update
     @book= Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)
+    if @book.update(book_params)
+       flash[:notice]="You have updated book successfully."
+       redirect_to book_path(@book)
+    else
+       render :edit
+    end
   end
 
   private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
 
   belongs_to :user
+  validates :title, presence: true
+  validates :body, length: {maximum: 20 }, presence: true
 end
