4614d509e53c59c0bcc4a974639f2011bd18a0b1
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-03-14 01:05:05 +0000

    [Finish]Bookers2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
-  
-  validates :name, length: { minimum: 2, maximum: 20 }
+
+  validates :name, length: {in:2..20}, uniqueness: true
   validates :introduction, length: {maximum: 50}
-  
+
   def get_profile_image(width,height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2023_03_12_104438) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
